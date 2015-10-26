f41b9ac5d89c66240ae62ba12af29b5e6a240184
Pradeep <pradeep.rao@amd.com>
2015-10-26 16:25:17 +0530

    Postcallback - Large 1D Interleaved C2C

--- src/library/generator.stockham.cpp
+++ src/library/generator.stockham.cpp
@@ -2889,7 +2889,7 @@ namespace StockhamGenerator
 
 					//Pass post-callback information to Pass object if its the last pass. 
 					//This will be used in single kernel transforms
-					if (params.fft_hasPostCallback && i == (nPasses - 1))
+					if (params.fft_hasPostCallback && i == (nPasses - 1) && !params.blockCompute)
 					{
 						passes[i].SetPostcallback(params.fft_hasPostCallback, params.fft_postCallback);
 					}
@@ -4100,7 +4100,7 @@ namespace StockhamGenerator
 							str += outBuf;
 							str += IterRegs("&"); 
 							
-							if (params.fft_hasPostCallback)
+							if (!blockCompute && params.fft_hasPostCallback)
 							{
 								str += ", post_userdata";
 
@@ -4176,9 +4176,27 @@ namespace StockhamGenerator
 
 						if( (blockComputeType == BCT_C2C) || (blockComputeType == BCT_R2C) )
 						{
-							str += "\t\t"; str += writeBuf; str += "[(me%"; str+= SztToStr(blockWidth); str += ") + ";
-							str += "(me/"; str+= SztToStr(blockWidth); str+= ")*"; str += SztToStr(params.fft_outStride[0]);
-							str += " + t*"; str += SztToStr(params.fft_outStride[0]*blockWGS/blockWidth); str += "] = R0"; str+= comp; str += ";\n";
+							if (blockComputeType == BCT_R2C && params.fft_hasPostCallback && outInterleaved)
+							{
+								writeBuf = (params.fft_placeness == CLFFT_INPLACE) ? "gb" : "gbOut";
+								
+								str += "\t\t"; str += params.fft_postCallback.funcname; str += "("; str += writeBuf; str += ", (";
+								str += outOffset; str += " + (me%"; str+= SztToStr(blockWidth); str += ") + ";
+								str += "(me/"; str+= SztToStr(blockWidth); str+= ")*"; str += SztToStr(params.fft_outStride[0]);
+								str += " + t*"; str += SztToStr(params.fft_outStride[0]*blockWGS/blockWidth); 
+								str += "), post_userdata, R0"; 
+								if (params.fft_postCallback.localMemSize > 0)
+								{
+									str += ", localmem";
+								}
+								str += ");\n";
+							}
+							else
+							{
+								str += "\t\t"; str += writeBuf; str += "[(me%"; str+= SztToStr(blockWidth); str += ") + ";
+								str += "(me/"; str+= SztToStr(blockWidth); str+= ")*"; str += SztToStr(params.fft_outStride[0]);
+								str += " + t*"; str += SztToStr(params.fft_outStride[0]*blockWGS/blockWidth); str += "] = R0"; str+= comp; str += ";\n";
+							}
 						}
 						else
 						{
